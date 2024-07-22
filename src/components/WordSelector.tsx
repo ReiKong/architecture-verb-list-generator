b29c978d420a547a4e46a13db6fe55d462640538
Rei Kong <reiikongs@gmail.com>
2024-07-22 11:49:18 +0800

    Configured for mobile

--- src/components/WordSelector.tsx
+++ src/components/WordSelector.tsx
@@ -22,7 +22,7 @@ export default function WordSelector({ onChange }: { onChange: (value: string) =
         <div className="">
             <Combobox value={selectedVerb} onChange={handleChange} onClose={() => setQuery('')}>
                 <ComboboxInput
-                    className="text-6xl md:text-7xl w-[374px] md:w-[444px] text-zinc-500 h-[66px] sm:h-20 hover:text-zinc-300 focus:text-zinc-300 font-bold tracking-tighter bg-transparent border-b-4 border-b-zinc-500 hover:border-b-zinc-300 focus:border-b-zinc-300 focus:outline-none"
+                    className="text-5xl sm:text-6xl md:text-7xl w-[294px] sm:w-[374px] md:w-[444px] text-zinc-500 h-[50px] sm:h-20 hover:text-zinc-300 focus:text-zinc-300 font-bold tracking-tighter bg-transparent border-b-4 border-b-zinc-500 hover:border-b-zinc-300 focus:border-b-zinc-300 focus:outline-none"
                     aria-label="Assignee"
                     displayValue={(verb: { id: number; word: string }) => verb?.word}
                     onChange={(event) => setQuery(event.target.value)}
