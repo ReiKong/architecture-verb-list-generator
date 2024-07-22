2872c6ca1e5e4ee731fe240d3169667bbdc4d2c1
Rei Kong <reiikongs@gmail.com>
2024-07-22 11:41:53 +0800

    Fixed typing issues

--- src/components/WordSelector.tsx
+++ src/components/WordSelector.tsx
@@ -2,7 +2,7 @@ import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headl
 import { mappedVerbs} from '../verbs'
 import { useState } from 'react'
 
-export default function Example({ onChange : String }) {
+export default function WordSelector({ onChange }: { onChange: (value: string) => void }) {
     const [selectedVerb, setSelectedVerb] = useState(mappedVerbs[1])
     const [query, setQuery] = useState('')
 
@@ -13,7 +13,7 @@ export default function Example({ onChange : String }) {
             return verb.word.toLowerCase().includes(query.toLowerCase())
             })
 
-    const handleChange = (verb : string) => {
+    const handleChange = (verb: { id: number; word: string }) => {
         setSelectedVerb(verb)
         onChange(verb.word)
     }
@@ -24,7 +24,7 @@ export default function Example({ onChange : String }) {
                 <ComboboxInput
                     className="text-6xl md:text-7xl w-[374px] md:w-[444px] text-zinc-500 h-[66px] sm:h-20 hover:text-zinc-300 focus:text-zinc-300 font-bold tracking-tighter bg-transparent border-b-4 border-b-zinc-500 hover:border-b-zinc-300 focus:border-b-zinc-300 focus:outline-none"
                     aria-label="Assignee"
-                    displayValue={(verb) => verb?.word}
+                    displayValue={(verb: { id: number; word: string }) => verb?.word}
                     onChange={(event) => setQuery(event.target.value)}
                 />
                 <ComboboxOptions anchor="bottom" className="absolute z-1 mt-2 w-[444px] bg-zinc-800 border border-zinc-700 empty:invisible tracking-tight">
