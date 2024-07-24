2845777ec43f7b47a3ceba74a54d27a4e8eb02cc
Rei Kong <reiikongs@gmail.com>
2024-07-24 15:42:08 +0800

    Readd anchor to enable scroll

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,9 @@ function App() {
         <h1 className="text-zinc-300">Architecture is the art of</h1>
         <div className="flex flex-row flex-wrap items-end justify-center">
           <WordSelector onChange={setCurrentVerb} />
-          <div className=" flex items-center justify-center">
+          <span className="flex items-center justify-center">
             <h1 className="border-b-4 border-b-transparent">&nbsp;and&nbsp;</h1>
-          </div>
+          </span>
           <div className="">
             <h1 className="border-b-4 border-b-zinc-300">{randomVerb}</h1>
           </div>
