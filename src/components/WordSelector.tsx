0ea81ea5e3e1863a8e105a8065f1b95316208e27
Rei Kong <reiikongs@gmail.com>
2024-07-24 16:14:23 +0800

    FINALLY FIXED COMBOBOX DROPDOWN OPTIONS POSITIONING ISSUE!

--- src/components/WordSelector.tsx
+++ src/components/WordSelector.tsx
@@ -19,22 +19,21 @@ export default function WordSelector({ onChange }: { onChange: (value: string) =
     }
 
     return (
-        <div className="relative">
+        <div className="relative w-fit">
             <Combobox value={selectedVerb} onChange={handleChange} onClose={() => setQuery('')}>
                 <ComboboxInput
                     className="text-5xl sm:text-6xl md:text-7xl w-[294px] sm:w-[374px] md:w-[444px] text-zinc-500 h-[50px] sm:h-20 hover:text-zinc-300 focus:text-zinc-300 font-bold tracking-tighter bg-transparent border-b-4 border-b-zinc-500 hover:border-b-zinc-300 focus:border-b-zinc-300 focus:outline-none rounded-[0px]"
-                    aria-label="Assignee"
+                    aria-label="Verb"
                     displayValue={(verb: { id: number; word: string }) => verb?.word}
                     onChange={(event) => setQuery(event.target.value)}
                 />
                 <ComboboxOptions
-                    anchor="bottom"
-                    className="absolute mt-2 w-[294px] sm:w-[374px] md:w-[444px] bg-zinc-800 border border-zinc-700 empty:invisible tracking-tight"
+                    className="absolute left-0 z-10 mt-2 w-[294px] sm:w-[374px] md:w-[444px] bg-zinc-800 border border-zinc-700 empty:invisible tracking-tight max-h-96 overflow-auto"
                 >
                     {filteredVerbs.map((verb) => (
-                    <ComboboxOption key={verb.id} value={verb} className="px-2 py-1 text-lg text-zinc-300 group flex gap-2 data-[focus]:bg-zinc-700">
-                        {verb.word}
-                    </ComboboxOption>
+                        <ComboboxOption key={verb.id} value={verb} className="px-2 py-1 text-lg text-zinc-300 group flex gap-2 data-[focus]:bg-zinc-700">
+                            {verb.word}
+                        </ComboboxOption>
                     ))}
                 </ComboboxOptions>
             </Combobox>
